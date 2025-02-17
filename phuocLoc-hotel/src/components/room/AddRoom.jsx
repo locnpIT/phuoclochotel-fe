5aa8f0e6df77eaf8914abf796c17ba7c7acac48e
locnpIT <npl2004qn@gmail.com>
2025-02-18 01:06:57 +0700

    upload

--- phuocLoc-hotel/src/components/room/AddRoom.jsx
+++ phuocLoc-hotel/src/components/room/AddRoom.jsx
@@ -57,8 +57,12 @@ const AddRoom = () => {
 
     } catch (error) {
       setErrorMessage(error.message)
-
     }
+    // sau 3 giây khi submit thì message = "" để bắt đầu submit cái khác
+    setTimeout(() => {
+      setSuccessMessage("")
+      setErrorMessage("")
+    }, 3000)
   }
 
   return (
@@ -67,6 +71,17 @@ const AddRoom = () => {
         <div className='row justify-content-center '>
           <div className='col-md-8 col-lg-6'>
             <h2 className='mt-5 mb-2'>Add a new room</h2>
+
+
+            {successMessage && (
+              <div className='alert alert-success fade show'>{successMessage}</div>
+            )}
+
+            {errorMessage && (
+              <div className='alert alert-danger fade show'>{errorMessage}</div>
+            )}
+
+
 
             <form onSubmit={handleSubmit}>
               <div className='mb-3'>
